da24e2c4d8cb07579767ac2ed32ed91696a3f4b6
tjewerynice <98793800+tjewerynice@users.noreply.github.com>
2022-03-25 20:07:17 -0500

    added subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 resource "aws_vpc" "tdvpc" {
    cidr_block = var.tdvpcidr
 }
+
+resource "aws_subnet" "prisub" {
+   vpc_id = "tdvpc"
+   cidr_block = var.subpricidr
+   tags {
+     Name = "prisub"
+   }
+}
--- var.tf
+++ var.tf
@@ -1,3 +1,7 @@
 variable "tdvpcidr" {
   default = "10.0.0.0/16"
 }
+
+variable "subpricidr" {
+  default = "10.0.1.0/24"
+}
